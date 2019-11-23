08e31c5caeebbc224f059f6f21edc1bcc7c1357d
rahul-izenda <rahul.rao@izenda.com>
2019-11-23 12:27:29 +0530

    Updates

---
+++ precache-manifest.e15e3da9a0615cda2686166351566458.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e7e4f64c2bb9054defe69c59e60e7e3",
+    "url": "/productdetail/index.html"
+  },
+  {
+    "revision": "baba2a330379d90141c4",
+    "url": "/productdetail/static/css/2.90045b7a.chunk.css"
+  },
+  {
+    "revision": "0362ad9a9bd5b05a7fb8",
+    "url": "/productdetail/static/css/main.e6213a40.chunk.css"
+  },
+  {
+    "revision": "baba2a330379d90141c4",
+    "url": "/productdetail/static/js/2.12db5bc1.chunk.js"
+  },
+  {
+    "revision": "0362ad9a9bd5b05a7fb8",
+    "url": "/productdetail/static/js/main.a1b030ee.chunk.js"
+  },
+  {
+    "revision": "a2b1ce8ae05f35973221",
+    "url": "/productdetail/static/js/runtime~main.e37d1e49.js"
+  },
+  {
+    "revision": "d98df3fdaef22638d47971fe2658e4d2",
+    "url": "/productdetail/static/media/logo.d98df3fd.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/productdetail/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/productdetail/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/productdetail/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/productdetail/static/media/slick.f97e3bbf.svg"
+  }
+]);
